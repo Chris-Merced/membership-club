78d82558c8fa029858f6305b3ec086cee2064811
Chris-Merced <148519061+Chris-Merced@users.noreply.github.com>
2024-10-10 15:47:24 -0500

    Increase input validation for user input

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -9,7 +9,10 @@ async function insertMember(req, res, next) {
   try {
     const user = await db.memberSearch(req.body.username);
     if (user) {
-      return res.render("sign-up", { errorMessage: "Username already exists" });
+      return res.render("sign-up", { errorMessage: "Sorry that username is taken!" });
+    }
+    if (req.body.password === "") {
+      return res.render("sign-up", { errorMessage: "Insufficient Password" });
     }
 
     bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
